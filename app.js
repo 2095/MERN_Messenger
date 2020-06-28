2fe91af70123c04bff98e5e4fb9aeee9a5a83bff
2095 <209511@mail.ru>
2020-06-28 21:21:52 +0800

    commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+const routes = require('./routes/auth.routes');
+
+const app = express();
+const port = config.get('port');
+const mongoUrl = config.get('mongoUrl');
+
+
+app.use('/api/auth', routes);
+
+async function start(){
+  try{
+    await mongoose.connect(mongoUrl, {
+       useNewUrlParser: true,
+       useUnifiedTopology: true
+    });
+    app.listen(port, () => console.log(`${port}`));
+  }
+  catch(e){
+    console.log('Server error', e.message);
+    process.exit(1);
+  }
+}
+
+start();
